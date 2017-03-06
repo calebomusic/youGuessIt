e7344f9ecc5d009b9942d37801e6913cf62d0871
calebomusic <calebomusic@gmail.com>
2017-03-05 16:17:47 -0800

    Add youGuessIt body

--- lib/youguessit.jsx
+++ lib/youguessit.jsx
@@ -1,9 +1,34 @@
+const generateGraph = require("./generateGraph.js");
 import React from 'react';
-import './youguessit.scss';
 
-
-export default React.createClass({
-  render: function() {
-    return <div className="youguessit">Hello World</div>;
+class YouGuessIt extends React.Component {
+  constructor(props) {
+    super(props)
   }
-});
+
+  componentDidMount() {
+    generateGraph(this.props.graphId, this.props.options);
+  }
+
+  render() {
+    return(
+      <div id={'graphWidget-' + this.props.graphId}>
+        <div className={'question-' + this.props.graphId}>
+          {this.props.question()}
+        </div>
+
+        <div id={this.props.graphId}></div>
+        <div id={'beforeGuess-' + this.props.graphId}
+             className={'beforeGuessComplete-' + this.props.graphId}>
+          {this.props.beforeGuess()}
+        </div>
+        <div className="hidden"
+             id={'answer-' + this.props.graphId}>
+            {this.props.answer()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default YouGuessIt;
